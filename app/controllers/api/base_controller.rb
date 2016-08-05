4240a63a0dddd90c2689359ba3213580dc53327c
TrungNT <hellovietnam93@gmail.com>
2016-08-05 09:51:39 +0700

    Update profile

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,15 +1,16 @@
 class Api::BaseController < ActionController::API
-  acts_as_token_authentication_handler_for User, {fallback: :none}
+  # acts_as_token_authentication_handler_for User, {fallback: :none}
+  # skip_before_action :verify_authenticity_token
+  include Authenticable
 
   respond_to :json
-  skip_before_action :verify_authenticity_token
 
-  private
-  def authenticate_user_from_token
-    entity = SimpleTokenAuthentication::Entity.new User
+  # private
+  # def authenticate_user_from_token
+  #   entity = SimpleTokenAuthentication::Entity.new User
 
-    if authenticate_entity_from_token!(entity).nil?
-      render json: {message: I18n.t("api.invalid_token"), data: {}, code: 0}, status: 401
-    end
-  end
+  #   if authenticate_entity_from_token!(entity).nil?
+  #     render json: {message: I18n.t("api.invalid_token"), data: {}, code: 0}, status: 401
+  #   end
+  # end
 end
