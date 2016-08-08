db77b0e34524e3c968020a1984ea01c9d336b330
tiennh1995 <nguyen.huu.tien@framgia.com>
2016-08-08 15:15:29 +0700

    Fix bug 3

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -15,7 +15,7 @@ class Api::SessionsController < Devise::SessionsController
       if user.actived?
         sign_in user, store: false
         user.update_attributes signed_in: true
-        render json: {message: t("api.sign_in.success"), data: {user: user}, code: 1}, status: :ok
+        render json: {message: t("api.sign_in.success"), data: {user: user}, code: 1}, status: 200
         return
       else
         render json: {message: t("api.sign_in.not_actived"), data: {}, code: 0},
@@ -30,9 +30,9 @@ class Api::SessionsController < Devise::SessionsController
     user = User.find_for_database_authentication phone_number: user_params[:phone_number]
     if user.signed_in? && user.authentication_token == user_params[:authentication_token]
       sign_out user
-      render json: {message: t("api.sign_out.success"), data: {}, code: 1}, status: :ok
+      render json: {message: t("api.sign_out.success"), data: {}, code: 1}, status: 200
     else
-      render json: {message: t("api.invalid_token"), data: {}, code: 0}, status: 401
+      render json: {message: t("api.invalid_token"), data: {}, code: 0}, status: 200
     end
   end
 
@@ -43,7 +43,7 @@ class Api::SessionsController < Devise::SessionsController
   end
 
   def invalid_login_attempt
-    render json: {message: t("api.sign_in.fails"), data: {}, code: 0}, status: 401
+    render json: {message: t("api.sign_in.fails"), data: {}, code: 0}, status: 200
   end
 
   private
