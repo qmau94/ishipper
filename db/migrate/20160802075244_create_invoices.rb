5d3e3daae2952180d354843d69e115fc21ca658d
hatd <ennino1234@gmail.com>
2016-08-08 10:35:52 +0700

    show shipper near

--- db/migrate/20160802075244_create_invoices.rb
+++ db/migrate/20160802075244_create_invoices.rb
@@ -2,9 +2,10 @@ class CreateInvoices < ActiveRecord::Migration[5.0]
   def change
     create_table :invoices do |t|
       t.string :name
-      t.string :address
+      t.string :address_start
       t.float :latitude_start
       t.float :longitude_start
+      t.string :address_finish
       t.float :latitude_finish
       t.float :longitude_finish
       t.string :delivery_time
