d98f9419d577d70975c98d4bc129a88e20c7cc9d
dat-hedspi <dat.hedspi@gmail.com>
2016-08-19 10:08:09 +0700

    list invoices of shop

---
+++ app/controllers/api/v1/shop/invoices_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::Shop::InvoicesController < Api::ShopBaseController
+  before_action :ensure_params_true, only: :index
+
+  def index
+    invoices = if params[:status] == "all"
+      current_user.invoices
+    else
+      current_user.invoices.send params[:status]
+    end
+    if invoices.any?
+      render json: {message: I18n.t("invoices.messages.get_invoices_success"),
+        data: {invoice: invoices}, code: 1}, status: 200
+    else
+      render json: {message: I18n.t("invoices.messages.get_invoices_fails"),
+        data: {}, code: 1}, status: 200
+    end
+  end
+
+  private
+  def ensure_params_true
+    statuses = Invoice.statuses
+    statuses["all"] = 6
+    unless (params[:status].nil? || params[:status].in?(statuses)) && params.has_key?(:status)
+      render json: {message: I18n.t("invoices.messages.missing_params"), data: {}, code: 0},
+        status: 422
+    end
+  end
+end
